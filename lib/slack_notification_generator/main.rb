32d32abfe2a40f36e48823f5f77656738b90be6c
Jason Hutchens <jasonhutchens@gmail.com>
2015-08-29 17:59:49 +0800

    move code into a class

--- lib/slack_notification_generator/main.rb
+++ lib/slack_notification_generator/main.rb
@@ -4,181 +4,185 @@ require 'chronic_duration'
 require 'date'
 require 'json'
 
-unless ENV['SLACK_HOOK']
-  puts 'you must define the "SLACK_HOOK" environment variable'
-  exit 1
-end
-
-env =
-  case ENV['CI_BRANCH']
-  when 'master'
-    'Production'
-  when 'develop'
-    'Staging'
-  when /^release/
-    'Release'
-  else
-    'Development'
-  end
-
-changelog = []
-
-this_tag = 'HEAD'
-prev_tag = `git describe --abbrev=0 --tags`.strip
-
-unless ARGV[0] == "HEAD"
-  this_tag = prev_tag
-  prev_tag = `git describe --abbrev=0 --tags #{this_tag}^`.strip
-end
-
-def extract_commits(blob)
-  commits = []
-  commit = nil
-  blob.split("\n").map(&:strip).each do |line|
-    case line
-    when /^commit ([0-9a-f]+)$/
-      commits << commit unless commit.nil?
-      commit = { hash: $1[0..6], skip: false }
-    when /^Author: ([a-zA-Z]+)/
-      commit[:author] = $1.downcase
-    when /^Date: +(.*)$/
-      commit[:date] = Date.parse($1)
-    when /^([A-Z]+-[0-9]+)/
-      commit[:issue] = $1
-      if line =~ /#time +([0-9][^\s]*)/
-        commit[:time] = $1
-      end
-    else
-      commit[:message] ||= line unless line.empty?
-      commit[:skip] = true if line =~ /\[skip ci\]/
+class SlackNotificationGenerator
+  def self.run
+    unless ENV['SLACK_HOOK']
+      puts 'you must define the "SLACK_HOOK" environment variable'
+      exit 1
     end
-  end
-  commits << commit unless commit.nil?
-  commits
-end
 
-def add_attachment(store, title, data)
-  return if data.nil? || data.length == 0
-  data.map! do |line|
-    line.split("\n").map(&:strip).join("\n  ")
-  end
-  store <<
-    {
-      title: title,
-      text: data.map { |l| "• #{l}" }.join("\n")
-    }
-end
-
-def format(commit)
-  commit[:authors] ||= [commit[:author]].compact
-  commit[:issues] ||= [commit[:issue]].compact
-  commit[:times] ||= [commit[:time]].compact
-  link =
-    if commit[:pull]
-      if ENV['SLACK_REPO']
-        "<#{ENV['SLACK_REPO']}/pull/#{commit[:pull]}|##{commit[:pull]}>"
+    env =
+      case ENV['CI_BRANCH']
+      when 'master'
+        'Production'
+      when 'develop'
+        'Staging'
+      when /^release/
+        'Release'
       else
-        "##{commit[:pull]}"
+        'Development'
       end
-    else
-      if ENV['SLACK_REPO']
-        "<#{ENV['SLACK_REPO']}/commit/#{commit[:hash]}|##{commit[:hash]}>"
-      else
-        "##{commit[:hash]}"
-      end
-    end
-  issues =
-    if commit[:issues].empty?
-      nil
-    else
-      commit[:issues].map do |issue|
-        if ENV['SLACK_JIRA']
-          "<#{ENV['SLACK_JIRA']}/browse/#{issue}|#{issue}>"
-        else
-          issue
-        end
-      end.join(', ')
-    end
-  time =
-    if commit[:times].empty?
-      nil
-    else
-      seconds =
-        commit[:times].map do |data|
-          ChronicDuration.parse(data)
-        end.reduce(:+)
-      ChronicDuration.output(seconds)
-    end
-  extras = [issues, commit[:authors].join(', '), time].compact.join(', ')
-  if commit[:message].length > 50
-    commit[:message] = commit[:message][0, 47] + "..."
-  end
-  <<-eos
-    #{link} #{commit[:message]} [#{extras}]
-  eos
-end
 
-commits = []
-commit = nil
-blob = `git log --merges #{this_tag} ^#{prev_tag}`
-blob.split("\n").map(&:strip).each do |line|
-  case line
-  when /^commit ([0-9a-f]+)$/
+    changelog = []
+
+    this_tag = 'HEAD'
+    prev_tag = `git describe --abbrev=0 --tags`.strip
+
+    unless ARGV[0] == "HEAD"
+      this_tag = prev_tag
+      prev_tag = `git describe --abbrev=0 --tags #{this_tag}^`.strip
+    end
+
+    commits = []
+    commit = nil
+    blob = `git log --merges #{this_tag} ^#{prev_tag}`
+    blob.split("\n").map(&:strip).each do |line|
+      case line
+      when /^commit ([0-9a-f]+)$/
+        commits << commit unless commit.nil?
+        commit = { hash: $1[0..6] }
+      when /^Merge: ([a-f0-9]+) ([a-f0-9]+)/
+        commit[:range] = ($1..$2)
+      when /^Author: ([a-zA-Z]+)/
+      when /^Date: +(.*)$/
+        commit[:date] = Date.parse($1)
+      when /^Merge pull request #([0-9]*) /
+        commit[:pull] = $1
+      else
+        commit[:message] ||= line unless line.empty?
+      end
+    end
     commits << commit unless commit.nil?
-    commit = { hash: $1[0..6] }
-  when /^Merge: ([a-f0-9]+) ([a-f0-9]+)/
-    commit[:range] = ($1..$2)
-  when /^Author: ([a-zA-Z]+)/
-  when /^Date: +(.*)$/
-    commit[:date] = Date.parse($1)
-  when /^Merge pull request #([0-9]*) /
-    commit[:pull] = $1
-  else
-    commit[:message] ||= line unless line.empty?
+
+    pull_requests = []
+    commit_filter = {}
+    commits.each do |commit|
+      next unless commit[:pull]
+      blob = `git log --no-merges #{commit[:range].last} ^#{commit[:range].first}`
+      commit[:authors] = []
+      commit[:issues] = []
+      commit[:times] = []
+      extract_commits(blob).each do |child|
+        commit_filter[child[:hash]] = true
+        commit[:authors] << child[:author]
+        commit[:issues] << child[:issue] if child[:issue]
+        commit[:times] << child[:time] if child[:time]
+      end
+      commit[:authors].sort!.uniq!
+      commit[:issues].sort!.uniq!
+      pull_requests << commit
+    end
+    pull_requests.map! { |commit| format(commit) }
+
+    blob = `git log --no-merges #{this_tag} ^#{prev_tag}`
+    other_commits = []
+    extract_commits(blob).each do |commit|
+      next if commit_filter[commit[:hash]] || commit[:skip]
+      other_commits << commit
+    end
+    other_commits.map! { |commit| format(commit) }
+
+    attachments = []
+    add_attachment(attachments, "Pull Requests", pull_requests)
+    add_attachment(attachments, "Other Commits", other_commits)
+    exit if attachments.length == 0
+
+    payload =
+      {
+        username: ENV['SLACK_USER'] || 'Notification',
+        icon_emoji: ENV['SLACK_ICON'] || ':bell:',
+        channel: ENV['SLACK_CHAN'] || '#general',
+        text: "*#{[ENV['SLACK_NAME'], env].compact.join(' ')} Release*",
+        attachments: attachments
+      }
+
+    `curl -X POST --data-urlencode 'payload=#{payload.to_json}' #{ENV['SLACK_HOOK']}`
+  end
+
+  def extract_commits(blob)
+    commits = []
+    commit = nil
+    blob.split("\n").map(&:strip).each do |line|
+      case line
+      when /^commit ([0-9a-f]+)$/
+        commits << commit unless commit.nil?
+        commit = { hash: $1[0..6], skip: false }
+      when /^Author: ([a-zA-Z]+)/
+        commit[:author] = $1.downcase
+      when /^Date: +(.*)$/
+        commit[:date] = Date.parse($1)
+      when /^([A-Z]+-[0-9]+)/
+        commit[:issue] = $1
+        if line =~ /#time +([0-9][^\s]*)/
+          commit[:time] = $1
+        end
+      else
+        commit[:message] ||= line unless line.empty?
+        commit[:skip] = true if line =~ /\[skip ci\]/
+      end
+    end
+    commits << commit unless commit.nil?
+    commits
+  end
+
+  def add_attachment(store, title, data)
+    return if data.nil? || data.length == 0
+    data.map! do |line|
+      line.split("\n").map(&:strip).join("\n  ")
+    end
+    store <<
+      {
+        title: title,
+        text: data.map { |l| "• #{l}" }.join("\n")
+      }
+  end
+
+  def format(commit)
+    commit[:authors] ||= [commit[:author]].compact
+    commit[:issues] ||= [commit[:issue]].compact
+    commit[:times] ||= [commit[:time]].compact
+    link =
+      if commit[:pull]
+        if ENV['SLACK_REPO']
+          "<#{ENV['SLACK_REPO']}/pull/#{commit[:pull]}|##{commit[:pull]}>"
+        else
+          "##{commit[:pull]}"
+        end
+      else
+        if ENV['SLACK_REPO']
+          "<#{ENV['SLACK_REPO']}/commit/#{commit[:hash]}|##{commit[:hash]}>"
+        else
+          "##{commit[:hash]}"
+        end
+      end
+    issues =
+      if commit[:issues].empty?
+        nil
+      else
+        commit[:issues].map do |issue|
+          if ENV['SLACK_JIRA']
+            "<#{ENV['SLACK_JIRA']}/browse/#{issue}|#{issue}>"
+          else
+            issue
+          end
+        end.join(', ')
+      end
+    time =
+      if commit[:times].empty?
+        nil
+      else
+        seconds =
+          commit[:times].map do |data|
+            ChronicDuration.parse(data)
+          end.reduce(:+)
+        ChronicDuration.output(seconds)
+      end
+    extras = [issues, commit[:authors].join(', '), time].compact.join(', ')
+    if commit[:message].length > 50
+      commit[:message] = commit[:message][0, 47] + "..."
+    end
+    <<-eos
+      #{link} #{commit[:message]} [#{extras}]
+    eos
   end
 end
-commits << commit unless commit.nil?
-
-pull_requests = []
-commit_filter = {}
-commits.each do |commit|
-  next unless commit[:pull]
-  blob = `git log --no-merges #{commit[:range].last} ^#{commit[:range].first}`
-  commit[:authors] = []
-  commit[:issues] = []
-  commit[:times] = []
-  extract_commits(blob).each do |child|
-    commit_filter[child[:hash]] = true
-    commit[:authors] << child[:author]
-    commit[:issues] << child[:issue] if child[:issue]
-    commit[:times] << child[:time] if child[:time]
-  end
-  commit[:authors].sort!.uniq!
-  commit[:issues].sort!.uniq!
-  pull_requests << commit
-end
-pull_requests.map! { |commit| format(commit) }
-
-blob = `git log --no-merges #{this_tag} ^#{prev_tag}`
-other_commits = []
-extract_commits(blob).each do |commit|
-  next if commit_filter[commit[:hash]] || commit[:skip]
-  other_commits << commit
-end
-other_commits.map! { |commit| format(commit) }
-
-attachments = []
-add_attachment(attachments, "Pull Requests", pull_requests)
-add_attachment(attachments, "Other Commits", other_commits)
-exit if attachments.length == 0
-
-payload =
-  {
-    username: ENV['SLACK_USER'] || 'Notification',
-    icon_emoji: ENV['SLACK_ICON'] || ':bell:',
-    channel: ENV['SLACK_CHAN'] || '#general',
-    text: "*#{[ENV['SLACK_NAME'], env].compact.join(' ')} Release*",
-    attachments: attachments
-  }
-
-`curl -X POST --data-urlencode 'payload=#{payload.to_json}' #{ENV['SLACK_HOOK']}`
